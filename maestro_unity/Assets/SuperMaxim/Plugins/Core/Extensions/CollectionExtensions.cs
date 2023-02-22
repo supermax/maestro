3616508e7c762e5f8c28da32f6b45b95148b9fe1
Max <maxim@supermaxim.com>
2023-02-22 12:02:41 +0000

    CollectionExtensions
    Added move useful functions

--- maestro_unity/Assets/SuperMaxim/Plugins/Core/Extensions/CollectionExtensions.cs
+++ maestro_unity/Assets/SuperMaxim/Plugins/Core/Extensions/CollectionExtensions.cs
@@ -16,32 +16,9 @@ namespace SuperMaxim.Core.Extensions
         /// <returns>The array of objects</returns>
         public static object[] ToObjectArray<T>(this T[] source)
         {
-            if(source.IsNullOrEmpty())
-            {
-                return null;
-            }
             var copy = new object[source.Length];
             Array.Copy(source, copy, source.Length);
             return copy;
-        }
-
-        /// <summary>
-        /// Check if collection is null or empty 
-        /// </summary>
-        /// <param name="source">The source collection</param>
-        /// <typeparam name="T">The item type</typeparam>
-        /// <returns>"true" in case array us null or empty</returns>
-        public static bool IsNullOrEmpty<T>(this ICollection<T> source)
-        {
-            if(source == null)
-            {
-                return true;
-            }
-            if(source.Count < 1)
-            {
-                return true;
-            }
-            return false;
         }
 
         /// <summary>
@@ -59,6 +36,42 @@ namespace SuperMaxim.Core.Extensions
         }
 
         /// <summary>
+        /// Iterate thru the array and execute the given action
+        /// </summary>
+        /// <remarks>
+        /// Using more effective for(i) loop instead of the expensive enumerator
+        /// </remarks>
+        /// <param name="collection">The source array</param>
+        /// <param name="action">The action to execute during the iteration</param>
+        /// <typeparam name="T">The type of the item</typeparam>
+        public static void ForEach<T>(this T[] collection, Action<T> action)
+        {
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var i = 0; i < collection.Length; i++)
+            {
+                action(collection[i]);
+            }
+        }
+
+        /// <summary>
+        /// Iterate thru the list and execute the given action
+        /// </summary>
+        /// <remarks>
+        /// Using more effective for(i) loop instead of the expensive enumerator
+        /// </remarks>
+        /// <param name="collection">The source list</param>
+        /// <param name="action">The action to execute during the iteration</param>
+        /// <typeparam name="T">The type of the item</typeparam>
+        public static void ForEach<T>(this IList<T> collection, Action<T> action)
+        {
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var i = 0; i < collection.Count; i++)
+            {
+                action(collection[i]);
+            }
+        }
+
+        /// <summary>
         /// Checks if string is null or empty
         /// </summary>
         /// <param name="source">The source string</param>
@@ -67,6 +80,85 @@ namespace SuperMaxim.Core.Extensions
         {
             var isEmpty = string.IsNullOrEmpty(source);
             return isEmpty;
+        }
+
+        /// <summary>
+        /// Check if collection is null or empty
+        /// </summary>
+        /// <param name="source">The source collection</param>
+        /// <typeparam name="T">The item type</typeparam>
+        /// <returns>"true" in case array us null or empty</returns>
+        public static bool IsNullOrEmpty<T>(this ICollection<T> source)
+        {
+            if(source == null)
+            {
+                return true;
+            }
+            return source.Count < 1;
+        }
+
+        /// <summary>
+        /// Returns 1st element in the array
+        /// </summary>
+        /// <param name="collection">The source array</param>
+        /// <typeparam name="T">The type of the array elements</typeparam>
+        /// <returns>1st element</returns>
+        public static T First<T>(this T[] collection)
+        {
+            if (collection.IsNullOrEmpty())
+            {
+                return default;
+            }
+            var first = collection[0];
+            return first;
+        }
+
+        /// <summary>
+        /// Returns 1st element in the array
+        /// </summary>
+        /// <param name="collection">The source array</param>
+        /// <typeparam name="T">The type of the array elements</typeparam>
+        /// <returns>1st element</returns>
+        public static T First<T>(this IList<T> collection)
+        {
+            if (collection.IsNullOrEmpty())
+            {
+                return default;
+            }
+            var first = collection[0];
+            return first;
+        }
+
+        /// <summary>
+        /// Returns last element in the array
+        /// </summary>
+        /// <param name="collection">The source array</param>
+        /// <typeparam name="T">The type of the array elements</typeparam>
+        /// <returns>Last element</returns>
+        public static T Last<T>(this T[] collection)
+        {
+            if (collection.IsNullOrEmpty())
+            {
+                return default;
+            }
+            var last = collection[^1];
+            return last;
+        }
+
+        /// <summary>
+        /// Returns last element in the array
+        /// </summary>
+        /// <param name="collection">The source array</param>
+        /// <typeparam name="T">The type of the array elements</typeparam>
+        /// <returns>Last element</returns>
+        public static T Last<T>(this IList<T> collection)
+        {
+            if (collection.IsNullOrEmpty())
+            {
+                return default;
+            }
+            var last = collection[^1];
+            return last;
         }
     }
 }
